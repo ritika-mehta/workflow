4ac6bfe3b888ce23954941c0a5683ebbba475c40
Ritika Mehta <71760324+ritika-mehta@users.noreply.github.com>
2021-03-05 19:04:49 +0530

    Add files via upload

---
+++ frontend/precache-manifest.39d6b5e9b7d30073475e16d30efbb45b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a6b9191a0e1c23d66ecde677ed5ae53",
+    "url": "/static/media/gallery4.9a6b9191.png"
+  },
+  {
+    "revision": "89f91d568002633c9bae",
+    "url": "/static/css/main.18ee571d.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a8e099de3b09df9fce3bf26be91552f2",
+    "url": "/static/media/curly-rope.a8e099de.svg"
+  },
+  {
+    "revision": "7d55db2c3dfde80bee13",
+    "url": "/static/js/2.ea0aeb2f.chunk.js"
+  },
+  {
+    "revision": "15323bd9a16d35e26ca5a92def24e0b2",
+    "url": "/static/media/flower-pot.15323bd9.png"
+  },
+  {
+    "revision": "3358c1d95f780a017176b2454e98c2ca",
+    "url": "/static/media/gallery1.3358c1d9.png"
+  },
+  {
+    "revision": "a8ef052aedfa8f521e95363249ccdce9",
+    "url": "/static/media/gallery2.a8ef052a.png"
+  },
+  {
+    "revision": "dec1d37f25437e0bde2b938e26dacbf8",
+    "url": "/static/media/gallery3.dec1d37f.png"
+  },
+  {
+    "revision": "89f91d568002633c9bae",
+    "url": "/static/js/main.c048f03b.chunk.js"
+  },
+  {
+    "revision": "ba006f77fee09d18cc19d42eea2e4d18",
+    "url": "/static/media/girl.ba006f77.png"
+  },
+  {
+    "revision": "d51fd4037fe805feceef97e54ec6806a",
+    "url": "/static/media/workmapLogo.d51fd403.png"
+  },
+  {
+    "revision": "d2a38ef6c3f5c25504e69ad1ea15eafb",
+    "url": "/static/media/idel-girl.d2a38ef6.png"
+  },
+  {
+    "revision": "d55d9b571272b3765536a83565a2be33",
+    "url": "/static/media/contribute-img.d55d9b57.png"
+  },
+  {
+    "revision": "08ae80a0ff3ea95c39bafea5809604da",
+    "url": "/static/media/idle-girl2.08ae80a0.png"
+  },
+  {
+    "revision": "a5609b8456307c2c4ed9073969154587",
+    "url": "/static/media/profile-img.a5609b84.png"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "7d55db2c3dfde80bee13",
+    "url": "/static/css/2.073e640b.chunk.css"
+  },
+  {
+    "revision": "b0b9662d1a94905799daab3a9db395fb",
+    "url": "/index.html"
+  }
+];
---
+++ frontend/precache-manifest.c33faed2f512b0f665c49d5ac9879214.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a6b9191a0e1c23d66ecde677ed5ae53",
+    "url": "/static/media/gallery4.9a6b9191.png"
+  },
+  {
+    "revision": "86030cbd1a13ac70df44",
+    "url": "/static/css/main.4039a3af.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a8e099de3b09df9fce3bf26be91552f2",
+    "url": "/static/media/curly-rope.a8e099de.svg"
+  },
+  {
+    "revision": "1ef23b8f5d3175ca27d6",
+    "url": "/static/js/2.004fbcf9.chunk.js"
+  },
+  {
+    "revision": "15323bd9a16d35e26ca5a92def24e0b2",
+    "url": "/static/media/flower-pot.15323bd9.png"
+  },
+  {
+    "revision": "3358c1d95f780a017176b2454e98c2ca",
+    "url": "/static/media/gallery1.3358c1d9.png"
+  },
+  {
+    "revision": "a8ef052aedfa8f521e95363249ccdce9",
+    "url": "/static/media/gallery2.a8ef052a.png"
+  },
+  {
+    "revision": "dec1d37f25437e0bde2b938e26dacbf8",
+    "url": "/static/media/gallery3.dec1d37f.png"
+  },
+  {
+    "revision": "86030cbd1a13ac70df44",
+    "url": "/static/js/main.ccdf9a16.chunk.js"
+  },
+  {
+    "revision": "ba006f77fee09d18cc19d42eea2e4d18",
+    "url": "/static/media/girl.ba006f77.png"
+  },
+  {
+    "revision": "d51fd4037fe805feceef97e54ec6806a",
+    "url": "/static/media/workmapLogo.d51fd403.png"
+  },
+  {
+    "revision": "d2a38ef6c3f5c25504e69ad1ea15eafb",
+    "url": "/static/media/idel-girl.d2a38ef6.png"
+  },
+  {
+    "revision": "d55d9b571272b3765536a83565a2be33",
+    "url": "/static/media/contribute-img.d55d9b57.png"
+  },
+  {
+    "revision": "08ae80a0ff3ea95c39bafea5809604da",
+    "url": "/static/media/idle-girl2.08ae80a0.png"
+  },
+  {
+    "revision": "a5609b8456307c2c4ed9073969154587",
+    "url": "/static/media/profile-img.a5609b84.png"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "1ef23b8f5d3175ca27d6",
+    "url": "/static/css/2.073e640b.chunk.css"
+  },
+  {
+    "revision": "d6163f0d3b84ea0f7209d76fba9e24be",
+    "url": "/index.html"
+  }
+];
